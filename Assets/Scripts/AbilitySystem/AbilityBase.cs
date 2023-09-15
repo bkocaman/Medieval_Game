4b69837acb34a972463c170dce8d0fa21967075d
bkocaman <berkkocaman@hotmail.com>
2023-09-15 15:06:11 +0300

    Dash Ability Power Added

---
+++ Assets/Scripts/AbilitySystem/AbilityBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class AbilityBase : MonoBehaviour
+{
+    public class MyFloatEvent : UnityEvent<float> { }
+    public MyFloatEvent OnAbilityUse = new MyFloatEvent();
+    [Header("Ability Info")]
+    public string title;
+    public float cooldownTime = 5;
+    private bool canUse = true;
+
+
+    public void TriggerAbility()
+    {    
+          OnAbilityUse.Invoke(cooldownTime);
+          Ability();
+          StartCooldown(); 
+    }
+    public abstract void Ability();
+    void StartCooldown()
+    {
+        StartCoroutine(Cooldown());
+        IEnumerator Cooldown()
+        {
+            canUse = false;
+            yield return new WaitForSeconds(cooldownTime);
+            canUse = true;
+        }
+    }
+}
---
+++ Assets/Scripts/AbilitySystem/DashAbility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using DG.Tweening;
+using Cinemachine;
+
+public class DashAbility : AbilityBase
+{
+    [Header("Connections")]
+    [SerializeField] private Animator animator = default;
+    [SerializeField] private CinemachineFreeLook originalCam = default;
+    [Header("Visuals")]
+    [SerializeField] private Renderer skinnedMesh = default;
+    [SerializeField] private ParticleSystem dashParticle = default;
+    public override void Ability()
+    {
+        animator.SetTrigger("Dash");
+        dashParticle.Play();
+
+        Sequence dash = DOTween.Sequence()
+        .Insert(0, transform.DOMove(transform.position + (transform.forward * 2), .2f))
+        .AppendCallback(() => dashParticle.Stop())
+        .Insert(0, skinnedMesh.material.DOFloat(1, "FresnelAmount", .1f))
+        .Append(skinnedMesh.material.DOFloat(0, "FresnelAmount", .35f));
+
+
+        DOVirtual.Float(40, 50, .1f, SetCameraFOV)
+            .OnComplete(() => DOVirtual.Float(50, 40, .5f, SetCameraFOV));
+    }
+
+    void SetCameraFOV(float fov)
+    {
+        originalCam.m_Lens.FieldOfView = fov;
+    }
+
+}
