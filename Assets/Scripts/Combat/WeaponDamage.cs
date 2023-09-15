835074806b0e5a558f3f3967fac8ec3ad337c739
bkocaman <berkkocaman@hotmail.com>
2023-09-15 11:54:24 +0300

    Combat System and Targetting Enemy Fixed

---
+++ Assets/Scripts/Combat/WeaponDamage.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    [SerializeField] private Collider myCollider;
+    private int damage;
+    private float knockback;
+    private List<Collider> alreadyCollidedWith = new();
+
+    private void OnEnable()
+    {
+        alreadyCollidedWith.Clear();
+    }
+
+    public void SetAttack(int damage, float knockback)
+    {
+        this.damage = damage;
+        this.knockback = knockback;
+    }
+
+  
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other == myCollider) { return; }
+        
+        if (alreadyCollidedWith.Contains(other)) { return; }
+
+        alreadyCollidedWith.Add(other);
+
+        if (other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damage);
+        } 
+
+        if(other.TryGetComponent<ForceReceiver>(out ForceReceiver forceReceiver))
+        {
+            Vector3 direction = (other.transform.position - myCollider.transform.position).normalized;
+            forceReceiver.AddForce(direction * knockback);
+        }  
+    }
+}
